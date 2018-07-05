1c1628040b28d719a5669f0de08229b3c65f6946
TheCodeSleyer <ofir3322@gmail.com>
2018-07-05 13:50:14 +0300

    add use of env service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() {
+    console.log(`on ${environment.production ? 'prod' : 'dev'}.`);
+  }
 }
